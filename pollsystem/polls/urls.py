c909e39edb70507edb5094698d1963570ba319de
Radhika Gupta <gupta.radhika61997@gmail.com>
2020-06-20 00:23:39 +0530

    added the urls of the app

--- pollsystem/polls/urls.py
+++ pollsystem/polls/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name = 'polls'
 
-urlpatterns=[
-    path('', views)
-
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name ='detail'),
+    path('<int:question_id>/results/', views.results, name ='results'),
+    path('<int:question_id>/voting/', views.voting, name ='voting'),
 ]
+
+
