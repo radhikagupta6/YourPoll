8e2e9796afa436d48535bf0b091769ec87db75fb
radhikagupta6 <tarugupta1997@gmail.com>
2020-08-06 23:15:49 +0530

    'app'

--- pollsystem/polls/urls.py
+++ pollsystem/polls/urls.py
@@ -5,9 +5,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name ='detail'),
-    path('<int:question_id>/results/', views.results, name ='results'),
-    path('<int:question_id>/voting/', views.voting, name ='voting'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/voting/', views.voting, name='voting'),
 ]
-
-
